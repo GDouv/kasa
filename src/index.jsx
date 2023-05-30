5ee752d71f9ac150c41137969bd68b559696ad0d
Guillaume Douv <gui.douvrin@gmail.com>
2023-05-30 11:47:52 +0200

    added logements.json dans src/datas and renamed App.css en Home.css

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./utils/style/index.css";
+import "./utils/styles/index.css";
 import Home from "./pages/Home";
 import reportWebVitals from "./reportWebVitals";
 
