8a5843a8b3159687c7359a775fad52ef96455a08
Guillaume Douv <gui.douvrin@gmail.com>
2023-05-30 11:18:35 +0200

    Added prop-types dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
+    plugins: ["react"],
+    rules: {
         "react/react-in-jsx-scope": "off",
-  "react/jsx-uses-react": "off",
-    }
-}
+        "react/jsx-uses-react": "off",
+    },
+};
