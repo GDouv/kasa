0215a1008f721d8368ee6f48e44ae873bdde9c4d
Guillaume Douv <gui.douvrin@gmail.com>
2023-05-30 12:33:05 +0200

    Added ReactRouter and "root" Route

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./utils/styles/index.css";
-import Home from "./pages/Home";
+import Root from "./routes/root";
 import reportWebVitals from "./reportWebVitals";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Home />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
 
