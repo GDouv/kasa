2f3c0d8dd831767fc1aa7b12a33bdc20237b84d3
Guillaume Douv <gui.douvrin@gmail.com>
2023-06-15 03:12:34 +0200

    Better code Collapse (conditional rendering)

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -5,58 +5,33 @@ import arrow from "../../assets/Vector.svg";
 
 export default function Collapse({ title, text }) {
     const [isOpen, setIsOpen] = useState(true);
-    return isOpen ? (
-        <div onClick={() => setIsOpen(false)} className={style.dropdown}>
-            <div className={style.title}>{title}</div>
-            <img
-                src={arrow}
-                alt="Flèche blanche vers le bas"
-                className={`${style.arrow} ${style["no-rotate"]}`}
-            />
-        </div>
-    ) : (
-        <>
-            <div onClick={() => setIsOpen(true)} className={style.dropdown}>
+
+    return (
+        <div className={style["dropdown-container"]}>
+            <div onClick={() => setIsOpen(!isOpen)} className={style.dropdown}>
                 <div className={style.title}>{title}</div>
                 <img
                     src={arrow}
-                    alt="Flèche blanche vers le haut"
-                    className={`${style.arrow} ${style.rotate}`}
+                    alt={
+                        isOpen
+                            ? "Flèche blanche vers le haut"
+                            : "Flèche blanche vers le bas"
+                    }
+                    className={
+                        isOpen
+                            ? `${style.arrow} ${style.rotate}`
+                            : `${style.arrow} ${style["no-rotate"]}`
+                    }
                 />
             </div>
-            <div className={style["text-container"]}>
-                <p>{text}</p>
-            </div>
-        </>
+            {isOpen && (
+                <div className={style["text-container"]}>
+                    <p>{text}</p>
+                </div>
+            )}
+        </div>
     );
 }
-
-// export default function Collapse({ title, text }) {
-//     const [isOpen, setIsOpen] = useState(true);
-//     return (
-//         <div>
-//             <div onClick={() => setIsOpen(true)} className={style.dropdown}>
-//                 <div className={style.title}>{title}</div>
-//                 <img
-//                     src={arrow}
-//                     alt="Flèche blanche vers le haut"
-//                     className={
-//                         isOpen
-//                             ? `${style.arrow} ${style.rotate}`
-//                             : `${style.arrow} ${style["no-rotate"]}`
-//                     }
-//                 />
-//             </div>
-//             <div
-//                 className={
-//                     isOpen ? style["text-container"] : style["display-none"]
-//                 }
-//             >
-//                 <p>{text}</p>
-//             </div>
-//         </div>
-//     );
-// }
 
 Collapse.propTypes = {
     title: PropTypes.string,
